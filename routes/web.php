a94660296650a5a4c7186a738d34d5e85a8bfe9e
Bieldeid <gabrieldeid.android@gmail.com>
2022-03-23 21:43:42 -0300

    Criado projeto laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::view('/jogos','jogos');
+
+// Route::get('/jogos', function(){
+//     return "Curso de Laravel";
+// });
+
+Route::view('/jogos', 'jogos',['name'=>'GTA']);
